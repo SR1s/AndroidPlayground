b2c37c1c5c2d35a9b55f6d6fd949bff4bdf5021b
SR1s <seemsnevermind@gmail.com>
2015-09-14 11:35:28 +0800

    添加获取Application级别的Context的方法

--- app/src/main/java/playground/android/me/sr1/androidplayground/App.java
+++ app/src/main/java/playground/android/me/sr1/androidplayground/App.java
@@ -1,6 +1,7 @@
 package playground.android.me.sr1.androidplayground;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 
@@ -8,9 +9,22 @@ import com.facebook.drawee.backends.pipeline.Fresco;
  * Created by sr1 on 15/8/11.
  */
 public class App extends Application {
+
+    private static Context sAppContext;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        sAppContext = this;
         Fresco.initialize(this);
+    }
+
+    /**
+     * 获取Application级别的Context
+     * 要注意在多进程的App上，获取到的并不是同一个实例。
+     * @return
+     */
+    public static Context getContext() {
+        return sAppContext;
     }
 }
