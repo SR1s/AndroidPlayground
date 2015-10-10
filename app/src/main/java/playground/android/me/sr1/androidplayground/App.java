9e94a74d2e8ff0e8be0f0b7b5ddecf174353bc0d
SR1s <seemsnevermind@gmail.com>
2015-10-10 15:01:50 +0800

    Bugly接入初始化

--- app/src/main/java/playground/android/me/sr1/androidplayground/App.java
+++ app/src/main/java/playground/android/me/sr1/androidplayground/App.java
@@ -4,8 +4,10 @@ import android.app.Application;
 import android.content.Context;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.tencent.bugly.crashreport.CrashReport;
 
 /**
+ * 应用的Application对象
  * Created by sr1 on 15/8/11.
  */
 public class App extends Application {
@@ -16,13 +18,14 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         sAppContext = this;
+        CrashReport.initCrashReport(this, "900009437", false);
         Fresco.initialize(this);
     }
 
     /**
      * 获取Application级别的Context
      * 要注意在多进程的App上，获取到的并不是同一个实例。
-     * @return
+     * @return 返回Application的Context
      */
     public static Context getContext() {
         return sAppContext;
